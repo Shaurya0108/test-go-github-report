85ed786fc68f94034d0e9e3d257da0c2cc04565c
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-06-19 12:22:28 -0500

    retrieve orgs /orgs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main
 go 1.22.1
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-github/v62 v62.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-github/github"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello, World!")
 }
 
+func orgsHandler(w http.ResponseWriter, r *http.Request) {
+	client := github.NewClient(nil)
+	orgs, _, err := client.Organizations.List(context.Background(), "Shaurya0108", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(orgs); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
-    http.HandleFunc("/", helloWorldHandler)
-    fmt.Println("Starting server at port 8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println(err)
-    }
+	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/orgs", orgsHandler)
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
